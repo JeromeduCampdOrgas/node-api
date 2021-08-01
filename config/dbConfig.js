5595bd9932aca5548b9da7abe7a308de4c73b294
Jerome <ducampdorgasjerome@gmail.com>
2021-08-01 06:36:33 +0200

    commit first

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.d581w.mongodb.net/orinoco",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+
+)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Failed to connect to MongoDB', err))

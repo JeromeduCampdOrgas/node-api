622a7a9f44c84894fa66c0e1dfbfaba915584a23
Jerome <ducampdorgasjerome@gmail.com>
2021-08-02 15:09:12 +0200

    gestion images

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,20 +1,26 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth.controller');
-const userController = require('../controllers/user.controller');
+const router = require("express").Router();
+const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
-//auth
+// auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logout);
 
-//user display: 'block'
-router.get('/', userController.getAllUsers)
-router.get('/:id', userController.userInfo);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+// user DB
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 //followers
-router.patch('/follow/:id', userController.follow);
-router.patch('/unfollow/:id', userController.unfollow);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
 
-module.exports = router
+// upload
+router.post("/upload", upload.single("file"), uploadController.uploadProfil);
+
+module.exports = router;

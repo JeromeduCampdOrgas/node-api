c53e28d83adfa58a3359ca8d5da25b910115a4fc
Jerome <ducampdorgasjerome@gmail.com>
2021-08-02 15:27:46 +0200

    cors

--- server.js
+++ server.js
@@ -1,43 +1,49 @@
 //variables
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
 
-require('dotenv').config({ path: './config/.env' })
-require('./config/dbConfig');
+require("dotenv").config({ path: "./config/.env" });
+require("./config/dbConfig");
 
-const { checkUser, requireAuth } = require('./middleware/auth.middleware');
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+
+const cors = require("cors");
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
 
-const mongoose = require('mongoose');
-mongoose.set('useFindAndModify', false);
-const cors = require('cors');
+app.use(cors({ corsOptions }));
 
+const mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(cors());
-
 //jwt
-app.get('*', checkUser);
-app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id)
-})
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 //Routes
-app.use('/api/user', userRoutes);
-app.use('/api/post', postRoutes);
-
-
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 //server
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-})
+  console.log(`Listening on port ${process.env.PORT}`);
+});

c6d5241ffcf9deb6d5db82b9edf5fc3430069d43
Jerome <ducampdorgasjerome@gmail.com>
2021-08-02 09:52:24 +0200

    crud post + like + unlike

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
 
 require('dotenv').config({ path: './config/.env' })
 require('./config/dbConfig');
@@ -32,7 +33,7 @@ app.get('/jwtid', requireAuth, (req, res) => {
 })
 //Routes
 app.use('/api/user', userRoutes);
-
+app.use('/api/post', postRoutes);
 
 
 

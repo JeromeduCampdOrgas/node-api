e3913d208b6eb5bfb9b858de0a525deed9ee3541
Jerome <ducampdorgasjerome@gmail.com>
2021-08-02 10:37:39 +0200

    crud comments

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,115 +1,191 @@
-const PostModel = require('../models/post.model');
-const UserModel = require('../models/user.model');
-const ObjectId = require('mongoose').Types.ObjectId;
-
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports.readPost = (req, res) => {
-    PostModel.find((err, docs) => {
-        if (!err) res.send(docs);
-        else console.log('Error to get data: ' + err)
-    })
-}
+  PostModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data: " + err);
+  }).sort({ createdAt: -1 }); //tri des posts du plus récent au plus ancien
+};
 
 module.exports.createPost = async (req, res) => {
-    const newPost = new PostModel({
-        posterId: req.body.posterId,
-        message: req.body.message,
-        video: req.body.video,
-        comments: [],
-        likers: []
-    })
-    try {
-        const post = await newPost.save();
-        return res.status(201).json(post);
-    } catch {
-        return res.status(400).send(err)
-    }
-}
+  const newPost = new PostModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    video: req.body.video,
+    comments: [],
+    likers: [],
+  });
+  try {
+    const post = await newPost.save();
+    return res.status(201).json(post);
+  } catch {
+    return res.status(400).send(err);
+  }
+};
 
 module.exports.updatePost = (req, res) => {
-    if (!ObjectId.isValid(req.params.id))//On vérifie que l'id passé en paramètre est correct
-        return res.status(400).send('Id unknown: ' + req.params.id);
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
 
-    const updatedRecord = { message: req.body.message }
+  const updatedRecord = { message: req.body.message };
 
-    PostModel.findByIdAndUpdate(
-        req.params.id,
-        { $set: updatedRecord },
-        { new: true },
-        (err, docs) => {
-            if (!err) res.send(docs);
-            else console.log('Update error: ' + err);
-        }
-    )
-}
+  PostModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Update error: " + err);
+    }
+  );
+};
 
 module.exports.deletePost = (req, res) => {
-    if (!ObjectId.isValid(req.params.id))//On vérifie que l'id passé en paramètre est correct
-        return res.status(400).send('Id unknown: ' + req.params.id);
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
 
-    PostModel.findByIdAndRemove(
-        req.params.id,
-        (err, docs) => {
-            if (!err) res.send(docs);
-            else log.console('Delete error: ' + err)
-        }
-    )
-}
+  PostModel.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else log.console("Delete error: " + err);
+  });
+};
 
 module.exports.likePost = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id))//On vérifie que l'id passé en paramètre est correct
-        return res.status(400).send('Id unknown: ' + req.params.id);
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
 
-    try {
-        await PostModel.findByIdAndUpdate(
-            req.params.id,
-            {
-                $addToSet: { likers: req.body.id }
-            },
-            { new: true },
-            (err, docs) => {
-                if (err) return res.status(401).send(err);
-            }
-        );
-        await UserModel.findByIdAndUpdate(
-            req.body.id,
-            { $addToSet: { likes: req.params.id } },
-            { new: true },
-            (err, docs) => {
-                if (!err) res.send(docs);
-                else return res.status(400).send(err);
-            }
-        )
-    } catch (err) {
-        return res.status(400).send(err);
-    }
-}
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $addToSet: { likers: req.body.id },
+      },
+      { new: true },
+      (err, docs) => {
+        if (err) return res.status(401).send(err);
+      }
+    );
+    await UserModel.findByIdAndUpdate(
+      req.body.id,
+      { $addToSet: { likes: req.params.id } },
+      { new: true },
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
 
 module.exports.unlikePost = async (req, res) => {
-        if (!ObjectId.isValid(req.params.id))//On vérifie que l'id passé en paramètre est correct
-        return res.status(400).send('Id unknown: ' + req.params.id);
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
 
-    try {
-        await PostModel.findByIdAndUpdate(
-            req.params.id,
-            {
-                $pull: { likers: req.body.id }
-            },
-            { new: true },
-            (err, docs) => {
-                if (err) return res.status(401).send(err);
-            }
-        );
-        await UserModel.findByIdAndUpdate(
-            req.body.id,
-            { $pull: { likes: req.params.id } },
-            { new: true },
-            (err, docs) => {
-                if (!err) res.send(docs);
-                else return res.status(400).send(err);
-            }
-        )
-    } catch (err) {
-        return res.status(400).send(err);
-    }
-}
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: { likers: req.body.id },
+      },
+      { new: true },
+      (err, docs) => {
+        if (err) return res.status(401).send(err);
+      }
+    );
+    await UserModel.findByIdAndUpdate(
+      req.body.id,
+      { $pull: { likes: req.params.id } },
+      { new: true },
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+//Comments
+module.exports.commentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.id,
+            commenterPseudo: req.body.pseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.editCommentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
+  try {
+    return PostModel.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) =>
+        comment._id.equals(req.body.commentId)
+      );
+      if (!theComment) return res.status(404).send("Comment not found");
+      else theComment.text = req.body.text;
+
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        return res.status(500).send(err);
+      });
+    });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.deleteCommentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    //On vérifie que l'id passé en paramètre est correct
+    return res.status(400).send("Id unknown: " + req.params.id);
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};

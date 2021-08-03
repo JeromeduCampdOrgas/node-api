2581977be2f80518e052aae118342ea0b591aa22
Jerome <ducampdorgasjerome@gmail.com>
2021-08-03 15:22:26 +0200

    logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
+import axios from "axios";
 
 const App = () => {
+  const [uid, setUid] = React.useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => setUid(res.data))
+        .catch((err) => console.log("No Token"));
+    };
+    fetchToken();
+  }, [uid]);
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Routes />
-    </div>
+    </UidContext.Provider>
   );
 };
 

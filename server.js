3028208dd5f4dd6c9793b8890f6a5909dcfb4cba
Jerome <ducampdorgasjerome@gmail.com>
2021-08-03 10:25:16 +0200

    corrections backend config db

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//variables
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
-
 require("dotenv").config({ path: "./config/.env" });
 require("./config/dbConfig");
-
 const { checkUser, requireAuth } = require("./middleware/auth.middleware");
-
 const cors = require("cors");
 
 const app = express();
@@ -23,27 +18,23 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
 };
-
-app.use(cors({ corsOptions }));
-
-const mongoose = require("mongoose");
-mongoose.set("useFindAndModify", false);
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
-//jwt
+// jwt
 app.get("*", checkUser);
 app.get("/jwtid", requireAuth, (req, res) => {
   res.status(200).send(res.locals.user._id);
 });
-//Routes
+
+// routes
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 
-//server
+// server
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });

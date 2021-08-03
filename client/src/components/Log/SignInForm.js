1b1a82b8a2a4683cd6d3fd17d579e6e46efe979f
Jerome <ducampdorgasjerome@gmail.com>
2021-08-03 09:26:58 +0200

    test

---
+++ client/src/components/Log/SignInForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {};
+
+  return <form action="" onSubmit={handleLogin} id="sign-up-form"></form>;
+};
+
+export default SignInForm;
